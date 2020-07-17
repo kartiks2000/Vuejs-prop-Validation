9324a9d53fb1774a1c3d5b40d3f3d932ce894fbf
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-17 17:06:49 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="kk">
+    <h1>Hello!! I am the parent....</h1>
+    <Child childname="kartik" age="19" country="USA"></Child>
+  </div>
+</template>
+
+<script>
+import child from './child';
+export default {
+  components:{
+    Child : child
+  },
+}
+</script>
+
+<style scoped>
+  h1{
+    color: red;
+  }
+  div{
+    margin: 3rem;
+  }
+  .kk{
+    background-color: cornsilk;
+    padding: 3rem;
+  }
+</style>>
+
+</style>
---
+++ src/child.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Hello!! I am the child....</h1>
+    <h2>my name is {{childname}} i am {{age}} years old. i live in {{country}}.</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props :{
+      childname : String,       //having single type
+      age : [String,Number],    //having multiple types
+      country :{
+        type : String,
+        required : true,
+        default : "India"
+      }
+  }
+}
+</script>
+
+<style scoped>
+    h1{
+        color: blue;
+    }
+    div{
+        background-color: burlywood;
+    }
+</style>>
+
